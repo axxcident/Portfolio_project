bc821bb57565dab703c58cb4a60b7c21b028c7cc
Axel Olivecrona <112471629+axxcident@users.noreply.github.com>
2022-12-14 22:16:23 +0100

    början på 2a video

---
+++ js/index.js
@@ -0,0 +1,10 @@
+/*   <!-- ============= Typing animation ============= --> */
+
+var typed = new Typed(".typing", {
+  strings: ["", "Web Designer", "Web ^1000 Developer", "Photoshopper", "Election plotter", "Front-end Engineer"],
+  typeSpeed: 55,
+  backSpeed: 100,
+  loop: true
+})
+// https://mattboldt.github.io/typed.js/
+// https://github.com/mattboldt/typed.js/
--- js/style-switcher.js
+++ js/style-switcher.js
@@ -4,7 +4,6 @@ const styleSwitcherToggle = document.querySelector(".style-switcher-toggler");
 
 styleSwitcherToggle.addEventListener("click", () => {
   document.querySelector(".style-switcher").classList.toggle("open");
-  console.log("here we go")
 });
 
 
@@ -25,9 +24,6 @@ window.addEventListener("scroll", () => {
 /*   <!-- ============= Theme Switcher ============= --> */
 const alternativeStyles = document.querySelectorAll(".alternate-style");
 
-console.log(typeof alternativeStyles);
-console.log(alternativeStyles);
-
 function setActiveStyle(color) {
   alternativeStyles.forEach(style => {
     if (color === style.getAttribute("title")) {
@@ -40,3 +36,19 @@ function setActiveStyle(color) {
     }
   });
 };
+
+/*   <!-- ============= Light / Dark mode ============= --> */
+const dayNight = document.querySelector(".day-night");
+dayNight.addEventListener("click", () => {
+  dayNight.querySelector("i").classList.toggle("fa-sun");
+  dayNight.querySelector("i").classList.toggle("fa-moon");
+  document.body.classList.toggle("dark");
+})
+window.addEventListener("load", () => {
+  if (document.body.classList.contains("dark")) {
+    dayNight.querySelector("i").classList.add("fa-sun");
+  }
+  else {
+    dayNight.querySelector("i").classList.add("fa-moon");
+  }
+})
