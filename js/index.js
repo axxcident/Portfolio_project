4b8ec63a1ae7f45012916a4b9e01eea4367f4144
Axel Olivecrona <112471629+axxcident@users.noreply.github.com>
2022-12-21 23:46:52 +0100

    Fixat knapp!

--- js/index.js
+++ js/index.js
@@ -8,3 +8,27 @@ var typed = new Typed(".typing", {
 })
 // https://mattboldt.github.io/typed.js/
 // https://github.com/mattboldt/typed.js/
+
+/*   <!-- ============= Mobile menu Aside BTN ============= --> */
+const nav = document.querySelector(".nav"),
+  navList = nav.querySelectorAll("li"),
+  totalNavList = navList.length;
+
+const navTogglerBTN = document.querySelector(".nav-toggler"),
+  aside = document.querySelector(".aside");
+
+navTogglerBTN.addEventListener("click", () => {
+  asideSectionToggle();
+});
+function asideSectionToggle() {
+  aside.classList.toggle("open");
+  navTogglerBTN.classList.toggle("openBTN");
+}
+
+for (let i = 0; i < totalNavList; i++) {
+  const a = navList[i].querySelector("a");
+  a.addEventListener("click", () => {
+    aside.classList.toggle("open");
+    navTogglerBTN.classList.toggle("openBTN");
+  })
+}
